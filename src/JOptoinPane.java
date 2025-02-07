b6d60072e990cb99f98be8b1c94c2205f848506e
mulayam-my18 <mulayamy71438@gmail.com>
2025-02-07 13:31:16 +0530

    Adding College Project

---
+++ src/JOptoinPane.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Mulayam Yadav
+ */
+class JOptoinPane {
+
+    static int showConfirmDialog(Object object, String do_You_Really_Want_To_Close_Application) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static int showConfirmDialog(Object object, String do_You_Really_Want_To_Close_Application, String select, int YES_NO_OPTION) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ src/Paragraph.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Mulayam Yadav
+ */
+class Paragraph {
+
+    Paragraph(String ______________________________bSc_ITPayme) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
